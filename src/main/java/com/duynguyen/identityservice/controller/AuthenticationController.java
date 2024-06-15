eb63d0e13b05c63f461f275a6c18f1d37437c9d2
Duy Nguyen <nguyenvanduy1816@gmail.com>
2024-06-15 13:26:49 +0700

    Add introspect token flow

--- src/main/java/com/duynguyen/identityservice/controller/AuthenticationController.java
+++ src/main/java/com/duynguyen/identityservice/controller/AuthenticationController.java
@@ -1,9 +1,12 @@
 package com.duynguyen.identityservice.controller;
 
 import com.duynguyen.identityservice.dto.request.AuthenticationRequest;
+import com.duynguyen.identityservice.dto.request.IntrospectRequest;
 import com.duynguyen.identityservice.dto.response.ApiResponse;
 import com.duynguyen.identityservice.dto.response.AuthenticationResponse;
+import com.duynguyen.identityservice.dto.response.IntrospectResponse;
 import com.duynguyen.identityservice.service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("auth")
@@ -23,6 +28,14 @@ public class AuthenticationController {
     public ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         var result = authenticationService.authenticate(request);
         return ApiResponse.<AuthenticationResponse>builder()
+                .result(result)
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    public ApiResponse<IntrospectResponse> introspect(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+        var result = authenticationService.introspectToken(request);
+        return ApiResponse.<IntrospectResponse>builder()
                 .result(result)
                 .build();
     }
--- src/main/java/com/duynguyen/identityservice/service/AuthenticationService.java
+++ src/main/java/com/duynguyen/identityservice/service/AuthenticationService.java
@@ -1,22 +1,28 @@
 package com.duynguyen.identityservice.service;
 
 import com.duynguyen.identityservice.dto.request.AuthenticationRequest;
+import com.duynguyen.identityservice.dto.request.IntrospectRequest;
 import com.duynguyen.identityservice.dto.response.AuthenticationResponse;
+import com.duynguyen.identityservice.dto.response.IntrospectResponse;
 import com.duynguyen.identityservice.exception.AppException;
 import com.duynguyen.identityservice.exception.ErrorCode;
 import com.duynguyen.identityservice.repository.UserRepository;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
@@ -26,26 +32,61 @@ import java.util.Date;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
 public class AuthenticationService {
-    @NonFinal
-    protected static final String SIGNER_KEY = "daqURcF8qCLgmiROhYwzS2CnxwPqymRzyLeQ1ExYFyhNJh0BKkoZn1EZcY8AprEo\n";
+
     UserRepository userRepository;
 
+    @NonFinal
+    @Value("${jwt.signerKey}")
+    protected String SIGNER_KEY;
+
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        var user = userRepository.findByUsername(request.getUsername()).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+        var user = userRepository
+                .findByUsername(request.getUsername())
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
         boolean authenticated = passwordEncoder.matches(request.getPassword(), user.getPassword());
+
         if (!authenticated) {
             throw new AppException(ErrorCode.UNAUTHENTICATED);
         }
 
         var token = generateToken(request.getUsername());
-        return AuthenticationResponse.builder().token(token).authenticated(true).build();
+        return AuthenticationResponse.builder()
+                .token(token)
+                .authenticated(true)
+                .build();
+    }
+
+    public IntrospectResponse introspectToken(IntrospectRequest request)
+            throws JOSEException, ParseException {
+        var token = request.getToken();
+
+        // verify token
+        JWSVerifier verifier = new MACVerifier(SIGNER_KEY.getBytes());
+
+        SignedJWT signedJWT = SignedJWT.parse(token);
+
+        Date expireTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+
+        var verified = signedJWT.verify(verifier);
+
+        return IntrospectResponse.builder()
+                .valid(verified && expireTime.after(new Date()))
+                .build();
     }
 
     private String generateToken(String username) {
+        log.info("Signer key: ", SIGNER_KEY.getBytes());
         JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
 
-        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder().subject(username).issuer("NguyenVanDuy").issueTime(new Date()).expirationTime(new Date(Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli())).claim("customClaim", "Custom").build();
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .subject(username)
+                .issuer("NguyenVanDuy")
+                .issueTime(new Date())
+                .expirationTime(new Date(Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()))
+                .claim("customClaim", "Custom")
+                .build();
 
         Payload payload = new Payload(jwtClaimsSet.toJSONObject());
         JWSObject jws0bject = new JWSObject(header, payload);
