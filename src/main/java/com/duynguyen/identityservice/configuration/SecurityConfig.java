f277f3fe3ca6e9dcfcc07cfef490fd651cbaf26a
Nguyen Van Duy <nguyenvanduy2286@gmail.com>
2024-06-17 11:33:03 +0700

    Allow request has token access resource

--- src/main/java/com/duynguyen/identityservice/configuration/SecurityConfig.java
+++ src/main/java/com/duynguyen/identityservice/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.duynguyen.identityservice.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,13 +10,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
     private final String[] PUBLIC_ENDPOINTS= {"/users", "/auth/login", "/auth/token", "/auth/introspect"};
+
+    @Value("${jwt.signerKey}")
+    private String signerKey;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,8 +34,21 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated()
         );
+        http.oauth2ResourceServer(
+                oauth2 -> oauth2.jwt(jwtConfigurer ->  jwtConfigurer.decoder(jwtDecoder()))
+        );
+
         http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
     }
 
     @Bean
